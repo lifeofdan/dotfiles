24e71e6278ccb178ce48938bb6926ae6daa6fcfc
Daniel Newman <contact@lifeofdan.me>
2026-08-11 23:49:35 -0400

    manage merge conflict

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,30 +1,54 @@
-{ config, pkgs, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 
 {
-    home.username = "nixofdan";
-    home.homeDirectory = "/home/nixofdan";
-    home.stateVersion = "26.05";
-    home.packages = with pkgs; [
-      neovim
-      zsh
-    ];
+  home.username = "nixofdan";
+  home.homeDirectory = "/home/nixofdan";
+  home.stateVersion = "26.05";
 
-    programs.neovim = {
-      enable = true;
+  # Add session paths for mason
+  home.sessionPath = [
+    "$HOME/.local/bin"
+    "$HOME/.local/share/nvim/mason/staging/nil/bin"
+  ];
 
-      extraPackages = with pkgs; [
-        tree-sitter
-        gcc
-      ];
+  home.packages = with pkgs; [
+    neovim
+    rustc
+    cargo
+    gcc
+    zsh
+    wezterm
+    rofi
+    waybar
+    lazygit
+  ];
+
+  home.file = {
+    ".config/hypr/hyprland.lua" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/hyprland/hyprland.lua";
     };
+  };
 
-    programs.git = {
-      enable = true;
-      settings = {
-        user = {
-          name = "Daniel Newman";
-          email = "contact@lifeofdan.me";
-        };
+  programs.ghostty = {
+    enable = true;
+
+    settings = {
+      theme = "Catppuccin Mocha";
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "Daniel Newman";
+        email = "contact@lifeofdan.me";
       };
     };
+  };
 }
